0f10ea0caa33e9756962eb74b9f20853c155f0fb
Tanya <45432959+tkoshurnikova@users.noreply.github.com>
2020-07-27 19:56:45 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  gulp.watch("source/js/*.js", gulp.series("html", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("scripts", "html", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
--- source/js/accordion.js
+++ source/js/accordion.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 (function () {
@@ -7,23 +6,31 @@
   if (pageFooter) {
     var accordionElements = pageFooter.querySelectorAll('.accordion-element');
 
+    var removeActiveClass = function () {
+      var openedElements = pageFooter.querySelectorAll('.accordion-element--opened');
+      if (openedElements) {
+        openedElements.forEach(function (element) {
+          element.classList.remove('accordion-element--opened');
+        });
+      }
+    };
+
     accordionElements.forEach(function (accordionElement) {
       accordionElement.classList.remove('accordion-element--no-js');
+
       var button = accordionElement.querySelector('button');
-      var sectionHeading = accordionElement.querySelector('h3');
 
       var toggleSection = function () {
-        accordionElement.classList.toggle('accordion-element--opened');
 
         if (accordionElement.classList.contains('accordion-element--opened')) {
-          button.innerHTML = '<span class="visually-hidden">Закрыть раздел</span><svg width="16" height="2" viewBox="0 0 16 2" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 0.5H16V1.5H0V0.5Z" fill="#C4C4C4"/></svg>';
+          accordionElement.classList.remove('accordion-element--opened');
         } else {
-          button.innerHTML = '<span class="visually-hidden">Открыть раздел</span><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.5 7.5V0H7.5V7.5H0V8.5H7.5V16H8.5V8.5H16V7.5H8.5Z" fill="#C4C4C4"/></svg>';
+          removeActiveClass();
+          accordionElement.classList.add('accordion-element--opened');
         }
       };
 
       button.addEventListener('click', toggleSection);
-      sectionHeading.addEventListener('click', toggleSection);
     });
   }
 })();
